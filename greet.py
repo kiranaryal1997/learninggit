4d9e1bad06241d15b5c6123a6a4e8f7d77de749f
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-21 17:46:15 +0545

    added translator module and requirements.txt file

--- greet.py
+++ greet.py
@@ -1,4 +1,8 @@
 from main import myGreetings
+from translate import Translator
+
+translator = Translator(to_lang='pt')
 
 for greets in myGreetings:
-    print(greets.title())
+    # print(greets.title())
+    print(translator.translate(greets).title())
