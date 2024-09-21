5ae79c3befef99699f805265f20dcda48b2ea051
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-21 17:15:23 +0545

    My first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#creating a list for different greetings
+myGreetings = ["Good Morning", "Good Afternoon", "Good Evening", "Good Day", "Safe flight"]
+# print(myGreetings)
+for greet in myGreetings:
+    print(greet)
