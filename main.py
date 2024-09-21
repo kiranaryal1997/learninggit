1c5c9b702b30d638c27db329d2f810fe6d7ace12
Kiran Aryal <kiranaryal1997@gmail.com>
2024-09-21 17:47:31 +0545

    updated for loop as comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #creating a list for different greetings
 myGreetings = ["good morning", "good afternoon", "good evening", "good day", "safe flight"]
 # print(myGreetings)
-for greet in myGreetings:
-    print(greet)
+# for greet in myGreetings:
+#     print(greet)
